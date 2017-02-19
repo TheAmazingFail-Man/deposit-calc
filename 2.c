98cbc51202465990706fd56da97705ffd2f5ded5
TheAmazingFail-Man <Sheggal@yandex.ru>
2017-02-19 23:37:05 +0700

    Version_0.2

--- 2.c
+++ 2.c
@@ -3,27 +3,8 @@
 int main()
 {
 	int d, m;
-	error:
 	printf("Enter the number days: ");
 	scanf("%d", &d);
 	printf("Enter the amount (k)");
 	scanf("%d", &m);
-	if(d > 365 || m < 10){
-		printf("Error404\n");
-		goto error;
-	}
-	if(m <= 100){
-		if(d <= 30){
-			m = m  * 0.9;
-		}
-		if(d > 30 && d <= 120){
-			m = m * 1.02;
-		}
-		if(d > 120 && d <= 240){
-			m = m * 1.06;
-		}
-		if(d > 240 && d <= 365){
-			m = m * 1.12;
-		}
-	}
 }
