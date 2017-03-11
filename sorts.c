eb0aa6e5a428ca033a48edc356f633617205d25d
Чедуров Александр Николаевич <is641s30@lwp01.csc.local>
2017-03-11 12:10:45 +0600

    lalalas666

--- sorts.c
+++ sorts.c
@@ -1,7 +1,6 @@
 #include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 double wtime()
 {
@@ -17,18 +16,13 @@ int getrand (int mini, int maxi)
 
 void countingsort (int counting[], int n)
 {
-	int i;
-    double t;
-
-    t = wtime();
-    
-	int min, max;
+	int i, min, max;
   	min = counting[0];
   	max = counting[0];
   	for (i = 1; i < n; i++) {
-  	  if ( counting[i] < min ) {
-  	  	min = counting[i];
-  	  } else if ( counting[i] > max ) {
+  		if ( counting[i] < min ) {
+  	  		min = counting[i];
+  	  	} else if ( counting[i] > max ) {
   	    	max = counting[i];
     	}
   	}
@@ -46,60 +40,99 @@ void countingsort (int counting[], int n)
     	  counting[z++] = i;
     	}
 	}
-    
-    t = wtime() - t;
-	printf ("\nCounting: ");
-	/*for (i = 0; i < n; i++) {
-		printf ("%d ", counting[i]);
-	}*/
-    printf ("\nElapsed time: %.20f sec.\n", t);  
 }
 
-void bubblesort (uint32_t *mas[], uint32_t n)
+void bubblesort (int mas[], int n)
 {
-    uint32_t i, temp, s = 1;
+    int i, temp, s = 1;
     while (s) {
         s = 0;
         for (i = 1; i < n; i++) {
-            if (*mas[i] < *mas[i - 1]) {
-                t = mas[i];
-                *mas[i] = *mas[i - 1];
-                *mas[i - 1] = t;
+            if (mas[i] < mas[i - 1]) {
+                temp = mas[i];
+                mas[i] = mas[i - 1];
+                mas[i - 1] = temp;
                 s = 1;
             }
         }
     }
 }
 
+int mergehelp (int mas[], int mashelp[], int l, int m, int r)
+{
+	int i, left, right;
+	for (i = l; i < r + 1; i++) {
+		mashelp[i] = mas[i];
+	}
+	left = l;
+	right = m + 1;
+	i = l;
+	while ((left <= m) && (right <= r)) {
+		if (mashelp[left] <= mashelp[right]) {
+			mas[i] = mashelp[left];
+			left++;
+		} else {
+			mas[i] = mashelp[right];
+			right++;
+		}
+	i++;
+	}			
+	while (left <= m) {
+		mas[i] = mashelp[left];
+		left++;
+		i++;
+	}		
+	while (right <= r) {
+		mas[i] = mashelp[right];
+		right++;
+		i++;		
+	}
+	
+	return 0;
+}
+
+int mergesort (int mas, int mashelp, int l, int r)
+{
+	int m;
+	if (l < r) {
+		m = (l + r) / 2;
+		mergesort (mas, mashelp, l, r);
+		mergesort (mas, mashelp, m + 1, r);
+		mergehelp (mas, mashelp, l, m, r);
+	}
+	
+	return 0;
+}
+
 int main()
 {
 	double time;
-	uint32_t n = atoi(argv[1]);
-	for (; n < 1000000; n += 50000) {
-		uint32_t *mas = (uint32_t*)malloc(n * sizeof(uint32_t));
+	for (int n = 50000; n < 1000000; n += 50000) {
+		int *mas = malloc(n * sizeof(int));
+		int *mashelp = malloc(n * sizeof(int));
 		for (int i = 0; i < n; i++) {
 			mas[i] = getrand(0, 100000);
 		}
 		time = wtime();
 		countingsort (mas, n);
 		time = wtime() - time;
-		FILE *file = fopen ("countingsort.txt", "a");
-		fprintf (file, "Size of the array: %d	Elapsed time: %.6f\n", n, time);
-		fclose (file);
+		FILE *file1 = fopen ("countingsort.dat", "w");
+		fprintf (file1, "Size of the array: %d	Elapsed time: %.6f\n", n, time);
+		fclose (file1);
 	
 		time = wtime();
 		bubblesort (mas, n);
 		time = wtime() - time;
-		FILE *file = fopen ("bubblesort.txt", "a");
-		fprintf (file, "Size of the array: %d	Elapsed time: %.6f\n", n, time);
-		fclose (file);
+		FILE *file2 = fopen ("bubblesort.dat", "w");
+		fprintf (file2, "Size of the array: %d	Elapsed time: %.6f\n", n, time);
+		fclose (file2);
 	
 		time = wtime();
-		mergesort (*mas, 0, n - 1);
+		mergesort (mas, mashelp, 0, n);
 		time = wtime() - time;
-		FILE *file = fopen ("mergesort.txt", "a");
-		fprintf (file, "Size of the array: %d	Elapsed time: %.6f\n", n, time);
-		fclose (file);
+		FILE *file3 = fopen ("mergesort.dat", "w");
+		fprintf (file3, "Size of the array: %d	Elapsed time: %.6f\n", n, time);
+		fclose (file3);
 	}
 	
 	return 0;
